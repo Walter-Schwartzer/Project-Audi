5edda275513e65c91df890d7b44d879c2514160e
walter <walaser80@gmail.com>
2023-07-27 13:30:05 -0300

    betaV3

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "../componentes/layout/Layout";
+import Layout from "../componentes/layout/layout";
 
 import { routes } from "./routes.js"
 
